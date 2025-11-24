5c3f3a434ca2150c4371ff696e3dc56d03559735
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-24 08:47:18 +0530

    Update CanvasEditor.jsx

--- components/CanvasEditor.jsx
+++ components/CanvasEditor.jsx
@@ -15,7 +15,7 @@ export default function CanvasEditor() {
 
   const [position, setPosition] = useState({
     x: 57,
-    y: canvasSize / 2 - imageHeight / 2 + 40,
+    y: canvasSize / 2 - imageHeight / 2 + 80,
   });
 
   const handleUpload = (e) => {
